ff3f231de24384b089e0b50ddc1ed490e634c1fb
ApriyantoChandra <herryliee17@gmail.com>
2023-07-09 21:40:49 +0700

    memperbaiki salah dan benerin di file global global_variable

--- lib/screen/add_post.dart
+++ lib/screen/add_post.dart
@@ -86,4 +86,3 @@ class AddPost extends StatelessWidget {
     );
   }
 }
-  
--- lib/utils/global_variable.dart
+++ lib/utils/global_variable.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:trendify_new/screen/add_post.dart';
 // import 'package:trendify_new/screen/add_post_screen.dart';
 import 'package:trendify_new/screen/chat.dart';
 import 'package:trendify_new/screen/feed_screen.dart';
@@ -11,7 +12,7 @@ const webScreenSize = 600;
 List<Widget> homeScreenItems = [
   const FeedScreen(),
   const SearchScreen(),
-  const Text('add post'),
+  const AddPost(),
   ChatPage(),
   ProfileScreen(
     uid: FirebaseAuth.instance.currentUser!.uid,
