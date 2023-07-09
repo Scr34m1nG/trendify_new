a6952ff49d22622f12d15503319ba7769ddd9397
ApriyantoChandra <herryliee17@gmail.com>
2023-07-09 21:21:25 +0700

    membuat add post pada fitur add post

--- lib/screen/add_post_screen.dart
+++ lib/screen/add_post_screen.dart
@@ -18,3 +18,71 @@ class AddPost extends StatelessWidget {
           ),
           backgroundColor: backgroundColor,
         ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          child: Column(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage(
+                      'assets/images/admin_post.jpg',
+                    ),
+                  ),
+                  SizedBox(width: 15),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'admin',
+                          style: TextStyle(color: fontColor),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: TextField(
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                              hintText: 'Enter Your Trends..',
+                              hintStyle: TextStyle(color: nonActiveColor),
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Post',
+                    style: TextStyle(
+                      color: hyperLinkColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: (null),
+                    icon: Icon(
+                      Icons.send,
+                      color: hyperLinkColor,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
