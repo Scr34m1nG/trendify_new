2dd1e82bf320ebfc52795ab9b1a0f24e5aa29f1f
Cloovyy <vannessv11@gmail.com>
2023-07-09 16:54:08 +0700

    update global_variable

--- lib/utils/global_variable.dart
+++ lib/utils/global_variable.dart
@@ -1,18 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:trendify_new/screen/add_post_screen.dart';
 import 'package:trendify_new/screen/feed_screen.dart';
-import 'package:trendify_new/screen/profile_screen.dart';
-import 'package:trendify_new/screen/search_screen.dart';
 
 const webScreenSize = 600;
 
 List<Widget> homeScreenItems = [
   const FeedScreen(),
-  const SearchScreen(),
-  const AddPostScreen(),
-  const Text('notifications'),
-  ProfileScreen(
-    uid: FirebaseAuth.instance.currentUser!.uid,
-  ),
 ];
