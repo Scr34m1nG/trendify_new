ec18f5f763c1db325b21ff60a6e38e8cef3cfe97
Adyatma Ruliff Brahmantyo_535210047 <adyatmaruliff@gmail.com>
2023-07-09 17:01:04 +0700

    update chat page

--- lib/utils/global_variable.dart
+++ lib/utils/global_variable.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:trendify_new/screen/add_post_screen.dart';
+import 'package:trendify_new/screen/chat.dart';
 import 'package:trendify_new/screen/feed_screen.dart';
 import 'package:trendify_new/screen/profile_screen.dart';
 import 'package:trendify_new/screen/search_screen.dart';
@@ -11,7 +12,7 @@ List<Widget> homeScreenItems = [
   const FeedScreen(),
   const SearchScreen(),
   const AddPostScreen(),
-  const Text('notifications'),
+  ChatPage(),
   ProfileScreen(
     uid: FirebaseAuth.instance.currentUser!.uid,
   ),
